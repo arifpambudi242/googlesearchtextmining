107e4320981ea02dfdd387de081775cecc509732
Arif Pambudi <33048602+arifpambudi242@users.noreply.github.com>
2021-12-09 09:07:44 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,104 +1,94 @@
-#!Env/bin/python
 import requests
 from bs4 import BeautifulSoup as bs
-from wordcloud import WordCloud, STOPWORDS
-import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
 import re
-import sys
 
-fileSentences = open('hasil.txt', 'w')
-stopword = open('stopword.txt', 'r').read()
-stopword = stopword.split('\n')
-
-
+positive_words = [w.strip('\n')
+                  for w in open('positive-words.txt', 'r').readlines()]
+negative_words = [w.strip('\n')
+                  for w in open('negative-words.txt', 'r').readlines()]
 
 
-# Main program
+class SentimentAnalisys:
+    def __init__(self, positive_words, negative_words):
+        self.__positive_words = positive_words
+        self.__negative_words = negative_words
+
+    def getSentiment(self, keyword):
+        urls = self.getUrls(keyword)
+        articles = self.getArticles(urls)
+        return 'Positive' if self.sentiment(articles) else 'Negative'
+
+    def getUrls(self, keyword):
+        print('Looking for Url.....')
+        listUrl = []
+        for x in range(5):
+            link = "https://google.com/search?q=%s&tbm=nws&source=web&lr=lang_en&start=%s" % (
+                keyword, x)
+            with requests.Session() as s:
+                try:
+                    html = s.get(link)
+                    soup = bs(html.text, 'html.parser')
+                    h = soup.find_all(['a'])
+                    for tag in h:
+                        url = tag.get('href')
+                        if 'http' in url and 'google.' not in url and 'youtube.' not in url:
+                            # getting url
+                            url = tag.get('href')
+                            # check sparator
+                            sparator = url.split('http')[0]
+                            if sparator != '':
+                                listUrl.append('%s' % url.split(sparator)[
+                                               1].split('&')[0].split('%3')[0])
+                        else:
+                            continue
+                except:
+                    continue
+        return listUrl
+
+    def getArticles(self, listUrl=None):
+        print('Getting Articles.....')
+        listArticle = []
+        try:
+            for url in listUrl:
+                with requests.Session() as s:
+                    html = s.get(url)
+                    if 'article' in html.text:
+                        soup = bs(html.text, 'html.parser')
+                        hasil = soup.find_all(['article'])
+                        for h in hasil:
+                            if h.text.strip() != '' and h.text.strip() != '\n':
+                                listArticle.append(h.text.strip().lower())
+                    else:
+                        continue
+            print('{} articles found'.format(len(listArticle)))
+            joinedArticle = ' '.join(listArticle)
+            # validating character
+            validArticle = re.sub("[^A-Za-z0-9" "]+", " ", joinedArticle)
+            wordArticle = validArticle.split(' ')
+            return [w for w in wordArticle if not w in stopwords.words('english')]
+        except Exception as e:
+            return False
+
+    def sentiment(self, articles):
+        print('getting sentiment from {} words'.format(len(articles)))
+        negative = 0
+        positive = 0
+        for text in articles:
+            if text in self.__positive_words:
+                positive += 1
+            if text in self.__negative_words:
+                negative += 1
+        return positive > negative
+
+
 def main():
-	keyword = 'Virus Corona'
-	url = cariUrl(keyword)
-	print('ditemukan %s link'%url['jumlah'])
-	if url['jumlah'] > 0:
-		hasil = grabArticle(url['urls'])
-		buatHasil(hasil)
-		buatPlot(hasil)
-	else:
-		print('tidak ada link apapun!')
-		sys.exit()
+    global positive_words
+    global negative_words
+    sentiment = SentimentAnalisys(positive_words, negative_words)
 
-
-
-
-# menemukan link article di search engine google
-def cariUrl(keyword=None):
-	print('Cari Url.....')
-	listUrl = []
-	for x in range(3):
-		link = "https://google.com/search?q=%s&start=%s"%(keyword,x)
-		with requests.Session() as s:
-			html = s.get(link)
-
-			soup = bs(html.text, 'html.parser')
-
-			h = soup.find_all(['a'])
-
-			for tag in h:
-				url = tag.get('href')
-				if 'http' in url:
-					# ambil url
-					url = tag.get('href')
-					# check sparator
-					sparator = url.split('http')[0]
-					if sparator != '':
-						listUrl.append('%s'%url.split(sparator)[1].split('&')[0])
-				else:
-					continue
-	return {'urls': listUrl, 'jumlah': len(listUrl)}
-
-
-def grabArticle(listUrl=None):
-	print('Grabbing Article.....')
-	listArticle = []
-	try:
-		for url in listUrl:
-			with requests.Session() as s:
-				html = s.get(url)
-				soup = bs(html.text, 'html.parser')
-				hasil = soup.find_all(['p'])
-				for h in hasil:
-					if h.text.strip() != '':
-						listArticle.append(h.text.strip().lower())
-		joinedArticle = ' '.join(listArticle)
-		# check ke validan karakter
-		validArticle = re.sub("[^A-Za-z0-9" "]+", " " , joinedArticle)
-
-		wordArticle = validArticle.split(' ')
-
-		validwordArticle = [w for w in wordArticle if not w in stopword]
-
-		return ' '.join(validwordArticle)
-	except:
-		pass
-
-
-def buatHasil(sentences=None):
-	print('Post Hasil to file!!.....')
-	fileSentences.write(sentences)
-	# for sentence in sentences:
-	# 	fileSentences.write(sentence+'\n')
-	fileSentences.close()
-
-	return '!done'
-
-def buatPlot(kata=None):
-	print('Generating Article!.....')
-	wordcloud = WordCloud(width=800, height=800, max_words=100, background_color='white').generate(kata)
-
-	# Display the generated image:
-	plt.imshow(wordcloud, interpolation='bilinear')
-	plt.axis("off")
-	plt.show()
+    print(sentiment.getSentiment('sesuatu'))
 
 
 if __name__ == '__main__':
-	main()
+    main()
